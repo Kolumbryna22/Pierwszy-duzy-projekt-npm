1c4472f723538241e3a19653639f8657d8ce7d0c
Dominik Wilusz <wilusz1@o2.pl>
2018-09-17 21:46:50 +0200

    Add simple add img

--- modules/server.js
+++ modules/server.js
@@ -4,29 +4,25 @@ var handlers = require('./handlers');
 
 function start() {
     function onRequest(request, response) {
-        console.log("Odebrano zapytanie.");
+        console.log("Odebrano zapytanie.".green);
+        console.log("Zapytanie " + request.url + " odebrane.");
         response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Pierwsze koty za płoty");
-        response.end();
+
+        switch (request.url) {
+            case '/':
+            case '/start':
+                handlers.welcome(request, response);
+                break;
+            case '/upload':
+                handlers.upload(request, response);
+                break;
+            case '/show':
+                handlers.show(request, response);
+                break;
+            default:
+                handlers.error(request, response);
+        }
     }
-
-    // function onRequest(request, response) {
-    //     console.log("Odebrano zapytanie.".green);
-    //     console.log("Zapytanie " + request.url + " odebrane.");
-    //     response.writeHead(200, {"Content-Type": "text/plain"});
-
-    //     switch (request.url) {
-    //         case '/':
-    //         case '/start':
-    //             handlers.welcome(request, response);
-    //             break;
-    //         case '/upload':
-    //             handlers.upload(request, response);
-    //             break;
-    //         default:
-    //             handlers.error(request, response);
-    //     }
-    // }
 
     http.createServer(onRequest).listen(9000);
     console.log("Uruchomiono server".green);
